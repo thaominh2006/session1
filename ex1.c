7b4b923cf5f3edc221730dfbe0e217f791f54504
thaominh2006 <dothaominh2401@gmail.com>
2025-06-16 09:27:21 +0700

    ex01

---
+++ ex1.c
@@ -0,0 +1,12 @@
+//
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("Hello\n");
+    }
+    return 0;
+}
+// Độ phức tạp của thuật toán: O(n) tại vì nó được lặp lại O(1) + O(1) + ... + O(1) = O(n)
